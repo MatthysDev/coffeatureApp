06d077aca8902b4d7c4baef893fb245e0ebaf323
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-21 21:06:29 +0100

    add producto and title page

--- App.js
+++ App.js
@@ -27,7 +27,175 @@ export default function App() {
           Co'Feature{" "}
         </Text>
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          backgroundColor: "#F78965",
+          height: "65%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            height: "15%",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: 25,
+            fontFamily: "Futura",
+            padding: 10,
+          }}
+        >
+          Nos Mixtures
+        </Text>
+        <View
+          style={{
+            height: "25%",
+            width: "100%",
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: "25%",
+            width: "100%",
+
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: "25%",
+            width: "100%",
+
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+          <View
+            style={{
+              width: "30%",
+
+              backgroundColor: "#EEEEEE",
+              flex: 1,
+              margin: "2%",
+            }}
+          >
+            <Text>Prod1</Text>
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#002D45",
+          height: "20%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "Rockwell",
+            fontSize: 35,
+            padding: 35,
+            margin: 30,
+          }}
+        >
+          {" "}
+          Bag{" "}
+        </Text>
+      </View>
+
       <StatusBar style="" />
     </View>
   );
